2be2b16237403191687fcbbaf31ce774acf82a4c
vishal5208 <vishald5208@gmail.com>
2024-09-07 18:50:17 +0530

    interacting with on chain program

---
+++ 4-send-ping-transaction.ts
@@ -0,0 +1,36 @@
+import * as web3 from "@solana/web3.js"
+import "dotenv/config"
+import { getKeypairFromEnvironment } from "@solana-developers/node-helpers"
+
+const payer = getKeypairFromEnvironment("SECRET_KEY")
+const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
+
+const PING_PROGRAM_ADDRESS = new web3.PublicKey("ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa")
+const PING_PROGRAM_DATA_ADDRESS = new web3.PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod")
+
+const transaction = new web3.Transaction()
+
+const instruction = new web3.TransactionInstruction({
+    keys : [
+        {
+            pubkey : PING_PROGRAM_DATA_ADDRESS,
+            isSigner : false, 
+            isWritable : true
+        }
+    ],
+    programId : PING_PROGRAM_ADDRESS
+})
+
+transaction.add(instruction)
+
+const signature = await web3.sendAndConfirmTransaction(
+    connection, 
+    transaction, 
+    [payer]
+)
+
+console.log(`✅ Transaction completed! Signature is ${signature}`);
+
+console.log(
+    `You can view your transaction on Solana Explorer at:\nhttps://explorer.solana.com/tx/${signature}?cluster=devnet`,
+  );
